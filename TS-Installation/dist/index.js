40480244d5fdff24d306e83400e8550cf146a0d9
amahim <arifbin71@gmail.com>
2025-12-06 16:36:27 +0600

    initialize TypeScript project with basic setup and configuration

---
+++ TS-Installation/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function Intro(name) {
+    return `Hello ${name}`;
+}
+const userName = "Mahimmm";
+console.log(Intro(userName));
+//# sourceMappingURL=index.js.map
---
+++ TS-Installation/src/index.ts
@@ -0,0 +1,6 @@
+function Intro(name: string): string {
+  return `Hello ${name}`;
+}
+
+const userName: string = "Mahimmm";
+console.log(Intro(userName));
